bb3ae292c6084ee9cf9bf2b21ed5ac2761e64258
XiaoLeeFeiDao <XiaoLeeFeiDao@yandex.com>
2023-04-02 21:19:23 +0800

    update 202304022120

--- cpp_learn/04_using声明和using编译指令.cpp
+++ cpp_learn/04_using声明和using编译指令.cpp
@@ -1,15 +1,13 @@
 /*
  * @Author: stone8419 XiaoLeeFeiDao@yandex.com
  * @Date: 2023-03-05 22:52:15
- * @LastEditors: stone8419 XiaoLeeFeiDao@yandex.com
- * @LastEditTime: 2023-03-06 07:12:10
+ * @LastEditors: XiaoLeeFeiDao XiaoLeeFeiDao@yandex.com
+ * @LastEditTime: 2023-04-02 21:12:12
  * @FilePath: /test_win11/cpp_learn/04_using声明和using编译指令.cpp
  * @Description:
  * Copyright (c) 2023 by ${git_name} email: ${git_email}, All Rights Reserved.
  */
-//
-// Created by 李磊 on 2023/3/5.
-//
+
 #include "cpp_learn_inner.h"
 #include <iostream>
 using namespace std;
@@ -18,6 +16,7 @@ namespace KingGlory
 {
     int sunwukongId = 1;
 }
+
 static void LocalTest01()
 {
     int sunwukongId = 4;
@@ -29,6 +28,7 @@ static void LocalTest01()
 
     cout << "sunwukongId = " << sunwukongId << endl;
 }
+
 void Test04()
 {
     LocalTest01();
